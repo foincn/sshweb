eb346c93892f2236fa728e74bb8103014ef30807
lijia.buptisc <lijia.buptisc@709a9af0-0abf-a729-42b0-3b141176820f>
2010-09-28 13:44:54 +0000

    tips 1

--- js/status.js
+++ js/status.js
@@ -10,6 +10,17 @@ $(document).ready(function() {
 	else{
 		$("form[name='pay']").submit();
 	}
-		
+
 	});
+	if($('#f').length)
+	{  $('#paid').removeClass('hide');
+		var offset = $('#n').offset();
+		$('.tip').removeClass('hide').css({'top': offset.top - 20 + 'px', 'left': offset.left+ 345 + 'px'});
+		}
+	else
+	{$('#notpaid').removeClass('hide');
+		var offset = $('#s').offset();
+		$('.tip').removeClass('hide').css({'top': offset.top - 20 + 'px', 'left': offset.left+ 160 + 'px'});
+		$('.p').css('border-color','#ccc #F0E68C #ccc #ccc');
+		}
 });// JavaScript Document
