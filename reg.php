4884a9eed634960a8bd58fc798888ab65895ff00
kecker.d2 <kecker.d2@gmail.com>
2011-04-15 12:50:00 +0000

    打开注册得密码

--- reg.php
+++ reg.php
@@ -29,6 +29,24 @@ if ($pass2 == '') {
 		$sql_checkip = "select * from ip where ip = '$ip'";
 		$result_checkip = mysql_query ( $sql_checkip );
 		$ip_in = mysql_fetch_array ( $result_checkip );
+		///////////
+		if ($pass != $pass2) {
+			echo "两次输入不一致";
+			$smarty->display ( "reg.html" );
+		} else {
+			$date = date ( "Y-m-d" );
+			$sql = "insert into `user` values (default , '$name' , '$pass2' , null ,null,'$taobao_name','$qq','$email','0','$date')";
+			$result = mysql_query ( $sql ) or die ( "wrong:" . mysql_error () );
+			$_SESSION ['name'] = $name;
+			
+			// insert the ip info
+			$sql = "insert into `ip` values (default , '$name' , '$ip' )";
+			$result = mysql_query ( $sql ) or die ( "wrong:" . mysql_error () );
+			
+			header ( "Location:status.php?method=reg" );
+		}
+		
+	/*  这部分去掉注释就能实现重复注册检查功能，并注掉上方注释到这行
 		if ('' == $ip_in [ip]) {
 			//加用户
 			if ($pass != $pass2) {
@@ -50,6 +68,7 @@ if ($pass2 == '') {
 		else{
 			echo "不能重复注册获得测试号，如果需要获得多个测试号请与站长联系：旺旺kecker，QQ58287926";
 		}
+*/
 	
 	} else {
 		echo "有相同的用户名存在";
